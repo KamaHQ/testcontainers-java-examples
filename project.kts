4f948d594c8bc0ce9faca3d31b1ec9fc2c8bde44
Sergei Egorov <bsideup@gmail.com>
2017-02-04 14:10:52 +0200

    Create project.kts

---
+++ project.kts
@@ -0,0 +1,41 @@
+import com.github.kama.*
+import com.github.kama.tasks.*
+
+flow {
+    echo("Starting execution ${executionId}")
+
+    node {
+        echo("All execution will be performed on ${node.id}")
+
+        exec("git clone https://github.com/KamaHQ/testcontainers-java-examples.git .")
+
+        echo("All cloned.")
+
+        parallel((0..5).map { i ->
+            flow {
+                echo("Processing #$i...")
+                sleep(3.second)
+                echo("Done #$i")
+            }
+        })
+
+        val answer = input("Continue?")
+        echo("Answer was: $answer")
+
+        if (answer) {
+            exec("gradle --no-daemon init")
+
+            dir("spring-boot") {
+                file("build.gradle", append = true, text = """
+                    test.testLogging {
+                        displayGranularity 1
+                        showStandardStreams = true
+                        events "STARTED", "PASSED", "FAILED", "SKIPPED"
+                    }
+                """)
+
+                exec("../gradlew --no-daemon --console=rich clean test")
+            }
+        }
+    }
+}
