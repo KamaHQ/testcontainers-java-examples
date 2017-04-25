c3f9a3e2f86c6e4234a109fc0d2b7dd29e5e6d9c
Sergei Egorov <bsideup@gmail.com>
2017-04-25 10:16:07 +0200

    Update project.kts

--- project.kts
+++ project.kts
@@ -14,7 +14,7 @@ flow {
 
         parallel((0..5).map { i ->
             flow {
-                echo("Processing #$i...")
+                echo("Processing #$i on ${node.id}...")
                 sleep(3.second)
                 echo("Done #$i")
             }
