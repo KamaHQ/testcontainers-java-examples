73757df487dcedfec763359ce95897b3478e6eb9
Sergei Egorov <bsideup@gmail.com>
2017-08-28 17:15:04 +0200

    Update project.kts

--- project.kts
+++ project.kts
@@ -15,7 +15,7 @@ flow {
         echo("Answer was: $answer")
 
         if (answer) {
-            exec("gradle --no-daemon --console=rich init makeItFail")
+            exec("gradle --no-daemon --console=rich init")
 
             dir("spring-boot") {
                 file("build.gradle", append = true, text = """
